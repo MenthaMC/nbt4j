8a24a85cd3b197c5e02978954b16cfeb6b9cf88e
xuyin <1573880184@qq.com>
2025-01-10 18:33:23 +0800

    用JunitPerf测试了下

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -4,11 +4,14 @@ plugins {
 
 dependencies {
     testImplementation(project(":main"))
+    testImplementation("com.github.houbb:junitperf:2.0.7")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
 
-    testImplementation("org.openjdk.jmh:jmh-core:1.33")
-    testAnnotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.33")
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.test {
--- test/src/test/java/me/coderfrish/test/OutputPerformanceTest.java
+++ test/src/test/java/me/coderfrish/test/OutputPerformanceTest.java
@@ -1,30 +1,18 @@
 package me.coderfrish.test;
 
+import com.github.houbb.junitperf.core.annotation.JunitPerfConfig;
+import com.github.houbb.junitperf.core.report.impl.ConsoleReporter;
+import com.github.houbb.junitperf.core.report.impl.HtmlReporter;
 import me.coderfrish.nbt.NBTOutput;
 import me.coderfrish.nbt.type.NBTCompound;
-import org.junit.jupiter.api.Test;
 
-import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 
-public class OutputTest {
-    @Test
-    public void test() {
-//        try(NBTOutput output = new NBTOutput(new FileOutputStream("D:\\NBT\\test\\src\\test\\resources\\test.nbt"))) {
-//            Map<String, Object> objectMap = new HashMap<>();
-//            objectMap.put("key", "value");
-//            objectMap.put("safa", "dfs");
-//            objectMap.put("aa", "aa");
-//            objectMap.put("aas", true);
-//
-//            output.write(objectMap);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-
+public class OutputPerformanceTest {
+    @JunitPerfConfig(threads = 10, warmUp = 1_000, duration = 30_000
+            , reporter = {HtmlReporter.class, ConsoleReporter.class})
+    public void benchmark() throws InterruptedException {
         try(NBTOutput output = new NBTOutput(Files.newOutputStream(Paths.get("D:\\NBT\\test\\src\\test\\resources\\test1.nbt")))) {
             NBTCompound object = new NBTCompound();
             object.put("name", "Frish2021");
